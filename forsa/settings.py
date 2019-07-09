1b869dda056c198327e471cb95d5cdda7875dd46
unknown <itsrashidalabri@gmail.com>
2019-07-09 10:02:12 +0400

    Add base directory value to settings file

--- forsa/settings.py
+++ forsa/settings.py
@@ -11,6 +11,7 @@ import os
 
 from configurations import Configuration, values
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 class Common(Configuration):
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -164,18 +165,5 @@ class Production(Staging):
     """
     The in-production settings.
     """
-    # Security
-    SESSION_COOKIE_SECURE = values.BooleanValue(True)
-    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_HSTS_SECONDS = values.IntegerValue(31536000)
-    SECURE_REDIRECT_EXEMPT = values.ListValue([])
-    SECURE_SSL_HOST = values.Value(None)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
-    SECURE_PROXY_SSL_HEADER = values.TupleValue(
-        ('HTTP_X_FORWARDED_PROTO', 'https')
-    )
-
-    DATABASES = values.DatabaseURLValue(environ_name='DATABASE_URL')
+    pass
 
