d1acaa2a5a40005e5ce0b1a86a88c68c86fc2f3d
unknown <itsrashidalabri@gmail.com>
2019-07-10 15:09:45 +0400

    Update base template with new header

--- wajiha/views.py
+++ wajiha/views.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse
 
@@ -14,6 +16,7 @@ from wajiha.forms import OpportunityCreationForm
 
 from django.contrib import messages
 
+
 class OpportunityListView(ListView):
     model = Opportunity
     template_name = "wajiha/opportunity_list.html"
@@ -21,8 +24,8 @@ class OpportunityListView(ListView):
 
     def get_queryset(self):
         search_string = self.request.GET.get('q', '')
-        return self.model.objects.filter(Q(hidden=False), 
-            Q(description__icontains=search_string) | Q(title__icontains=search_string))
+        return self.model.objects.filter(Q(hidden=False),
+                                         Q(description__icontains=search_string) | Q(title__icontains=search_string))
 
 
 class OpportunityDetailView(DetailView):
@@ -30,14 +33,20 @@ class OpportunityDetailView(DetailView):
     template_name = "wajiha/opportunity_detail.html"
 
     def get(self, request, *args, **kwargs):
-        opportunity = get_object_or_404(self.model, pk=kwargs['pk'], slug=kwargs['slug'])
-        context = {'opportunity': opportunity, 'similar_opportunities': Opportunity.objects.filter(hidden=False)}
+        opportunity = get_object_or_404(
+            self.model, pk=kwargs['pk'], slug=kwargs['slug'])
+        similar_opportunities = self.model.objects.filter(
+            category=opportunity.category, hidden=False).exclude(id=opportunity.id)[:3]
+        shuffle(similar_opportunities)
+        context = {'opportunity': opportunity,
+                   'similar_opportunities': similar_opportunities}
         return render(request, self.template_name, context)
+
 
 class OpportunityCreationView(FormView):
     template_name = "wajiha/opportunity_create.html"
     form_class = OpportunityCreationForm
-    
+
     def get_success_url(self):
         return reverse('wajiha:opportunity_create_success', current_app=self.request.resolver_match.namespace)
 
@@ -48,6 +57,7 @@ class OpportunityCreationView(FormView):
 
 class OpportunityCreationSuccessView(TemplateView):
     template_name = 'wajiha/opportunity_create_success.html'
+
 
 class ContactView(TemplateView):
     template_name = 'wajiha/contact.html'
